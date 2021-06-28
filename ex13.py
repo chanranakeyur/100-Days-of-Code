20855852371a56bce7f58be83f44028f48a1c730
keyurchandarana <chandaranakeyur@gmail.com>
2021-06-28 20:38:39 +0530

    28/06/2021

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script,first,second,third = argv
+
+print("This is Script",script)
+print("This is First",first)
+print("This is Second",second)
+print("This is Third",third)
