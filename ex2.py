10cee0fe22288d95e2d401d0ccfc1f8e05773ebe
keyurchandarana <chandaranakeyur@gmail.com>
2021-06-28 20:28:17 +0530

    First push

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# Hello this is comment please ignore this
+# Start of code form below
+
+print("Hello World!")
+print("I could have code like this")
+
+# print("This code will not run")
+print("This will run")
+
+# End your code above
