36b0f284a063087a6ae9ca4f1015a65c46504ee2
keyurchandarana <chandaranakeyur@gmail.com>
2021-06-28 20:58:11 +0530

    push

--- ex13.py
+++ ex13.py
@@ -6,3 +6,8 @@ print("This is Script",script)
 print("This is First",first)
 print("This is Second",second)
 print("This is Third",third)
+
+firstName = input("Please enter your First Name : ")
+lastName = input("Please enter your Last Name : ")
+
+print("My name is : ",firstName + " " + lastName)
