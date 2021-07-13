01752eed20aeae30b523ce135f21967c6133339d
keyurchandarana <chandaranakeyur@gmail.com>
2021-07-13 19:20:51 +0530

    13/07/2021

---
+++ day-2-end.py
@@ -0,0 +1,4 @@
+num_char = len(input("What is your name?"))
+print(f"Your name has {num_char} characters.")
+
+print(type(num_char))
---
+++ day-2-start.py
@@ -0,0 +1,22 @@
+#Data types
+
+#String
+
+print("Hello"[0])
+print("Hello"[4])
+print("Hello " + "World")
+
+#Integer
+
+print(123 + 456)
+print(123_456_789)
+
+#Float
+
+Pi = 3.14159
+print(Pi)
+
+#Boolean
+
+True
+False
